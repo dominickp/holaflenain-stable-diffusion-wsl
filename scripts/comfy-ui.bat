717bbea83dca456bcdb616366454bc0b845005b8
Dom <dom@dominick.cc>
2023-12-09 10:42:20 -0500

    Update scripts

--- scripts/comfy-ui.bat
+++ scripts/comfy-ui.bat
@@ -1,4 +1,17 @@
-dir  C:\Users\Dom\Documents\GitHub\stable-diffusion-ui
-# docker-compose down
+@echo off
+setlocal enabledelayedexpansion
+
+echo Launching Docker Desktop
+timeout /t 3
+"C:\Program Files\Docker\Docker\Docker Desktop.exe"
+echo Launching Firefox 
 "C:\Program Files\Mozilla Firefox\firefox.exe" -private-window http://localhost:9905/
+
+:retry
+dir C:\Users\Dom\Documents\GitHub\stable-diffusion-ui
 docker-compose up comfy-ui
+
+echo.
+echo Press any key to retry, or press Ctrl+C to exit.
+pause >nul
+goto :retry
--- scripts/lama-cleaner.bat
+++ scripts/lama-cleaner.bat
@@ -1,4 +1,17 @@
-dir  C:\Users\Dom\Documents\GitHub\stable-diffusion-ui
-# docker-compose down
+@echo off
+setlocal enabledelayedexpansion
+
+echo Launching Docker Desktop
+timeout /t 3
+"C:\Program Files\Docker\Docker\Docker Desktop.exe"
+echo Launching Firefox 
 "C:\Program Files\Mozilla Firefox\firefox.exe" -private-window http://localhost:9950/
+
+:retry
+dir C:\Users\Dom\Documents\GitHub\stable-diffusion-ui
 docker-compose up lama-cleaner
+
+echo.
+echo Press any key to retry, or press Ctrl+C to exit.
+pause >nul
+goto :retry
